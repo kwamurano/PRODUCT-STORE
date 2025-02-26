91a285c9fccb1ca4eee2a4855d4951cdcf5dec73
kwality-ops <kwamu.ifeanyi@naijaprime.ng>
2025-02-26 13:06:54 +0100

    new file

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use("/api/products", productsRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "FRONTEND/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "FRONTEND", "dist", "index.html"));
   });
 }
 
